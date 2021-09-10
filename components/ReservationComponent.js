3d0aa9761e5b644944a932b042ea43e70f6c0afb
dbto1970 <donbartojunior@gmail.com>
2021-09-10 15:32:32 -0400

    branch for workshop3 initial commit

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { View, Text, ScrollView, StyleSheet, Switch, 
-    Picker, 
-    Button, Modal } from 'react-native';
+    Picker, Button, Modal } from 'react-native';
 import * as Animatable from 'react-native-animatable';
 import DateTimePicker from '@react-native-community/datetimepicker';
 
